e79aa7ba51fe6196845d4abbf6386035abf951f6
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-08-14 11:36:35 +0530

    segregating category from whole data

--- src/client/sagas/sagas.js
+++ src/client/sagas/sagas.js
@@ -1,10 +1,29 @@
 import { takeEvery, put, all } from 'redux-saga/effects'
 
+var category = [];
+
+function isPresent(categoryName){
+    for(let i=0; i<category.length; i++){
+        if(categoryName === category[i])
+            return true;
+    }
+    return  false
+}
+
+function fillCategory(wholeData){
+    for(let i=0; i<wholeData.length; i++){
+        if(isPresent(wholeData[i].category))
+            continue
+        category.push(wholeData[i].category)
+    }
+}
 function* fetchData(){
-    const json = yield fetch('https://api.myjson.com/bins/kjl6h')
+    const wholeData = yield fetch('https://api.myjson.com/bins/kjl6h')
     .then(response => response.json()); 
-    console.log(json)
-    yield put({ type: "RECEIVED_DATA", payload: json, });
+    console.log(wholeData)
+    fillCategory(wholeData)
+    console.log(category)
+    yield put({ type: "RECEIVED_DATA", payload: [wholeData,category] });
 }
 
 function* fetchActions() {
