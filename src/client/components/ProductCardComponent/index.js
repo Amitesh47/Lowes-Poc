9ad8dde53c352ec959b87a9a1364ad17ac061080
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-08-14 16:00:15 +0530

    added star

--- src/client/components/ProductCardComponent/index.js
+++ src/client/components/ProductCardComponent/index.js
@@ -13,7 +13,8 @@ import {
   ProductImage,
   ProductConatiner,
   CountContainer,
-  CartButtonImage
+  CartButtonImage,
+  Star
 } from "./styles";
 
 const ProductCard = (props) => {
@@ -23,30 +24,37 @@ const ProductCard = (props) => {
         {/* <h3 data-test="H3Category"></h3> */}
         <Card style={{ width: "16.5rem" }}>
           <Card.Body data-test="ProductCard">
-            <ProductImage src={props.thumbnail}data-test="ProductImage" />
+            <ProductImage src={props.thumbnail} data-test="ProductImage" />
             <ProductTitle data-test="ProductTitle">{props.name}</ProductTitle>
             <ProductDesc data-test="ProductDesc">{props.description}</ProductDesc>
-            <ProductRatings data-test="ProductRatings">({props.rating})</ProductRatings>
-            <ProductPrice data-test="ProductCost">{props.price}</ProductPrice>
-            <div>
-              <AddToCartButon data-test="AddToCartButton">Add to Cart</AddToCartButon>
-              <CountContainer>
-                <CartCountButton>
-                  <CartButtonImage data-test="DecrementCountButton" src="../../../../utils/assets/subIcon.png" alt="Remove From Cart" />
-                  {/* <CartButtonImage src={require("../../../../utils/assets/subIcon.png")} alt="Remove From Cart" />                             */}
-                </CartCountButton>
-                <CartCount>1</CartCount>
-                <CartCountButton>
-                  {/* <CartButtonImage src={require("../../../../utils/assets/addIcon.png")} alt="Add To Cart" />                  */}
-                  <CartButtonImage data-test="IncrementCountButton" src="../../../../utils/assets/addIcon.png" alt="Add To Cart" />
-                </CartCountButton>
-              </CountContainer>
-            </div>
+
+            <p><Star src="../../../../utils/assets/star.png"/>
+              <Star src="../../../../utils/assets/star.png"/>
+                <Star src="../../../../utils/assets/star.png"/>
+                <Star src="../../../../utils/assets/star.png"/>
+                <Star src="../../../../utils/assets/star.png"/>
+                      <ProductRatings data-test="ProductRatings">({props.rating})</ProductRatings></p>
+
+                    <ProductPrice data-test="ProductCost">{props.price}</ProductPrice>
+                    <div>
+                      <AddToCartButon data-test="AddToCartButton">Add to Cart</AddToCartButon>
+                      <CountContainer>
+                        <CartCountButton>
+                          <CartButtonImage data-test="DecrementCountButton" src="../../../../utils/assets/subIcon.png" alt="Remove From Cart" />
+                          {/* <CartButtonImage src={require("../../../../utils/assets/subIcon.png")} alt="Remove From Cart" />                             */}
+                        </CartCountButton>
+                        <CartCount>1</CartCount>
+                        <CartCountButton>
+                          {/* <CartButtonImage src={require("../../../../utils/assets/addIcon.png")} alt="Add To Cart" />                  */}
+                          <CartButtonImage data-test="IncrementCountButton" src="../../../../utils/assets/addIcon.png" alt="Add To Cart" />
+                        </CartCountButton>
+                      </CountContainer>
+                    </div>
           </Card.Body>
         </Card>
       </ProductConatiner>
     </Fragment>
-  );
-};
-
-export default ProductCard;
+              );
+            };
+            
+            export default ProductCard;
