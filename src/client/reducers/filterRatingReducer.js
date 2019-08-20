2190f13302c00b58eceab6b0305149ee3afb2c6b
Anurag Hakeem <anuhakee@publicisgroupe.net>
2019-08-20 16:09:31 +0530

    Fixes in filter comp and remaining filter reducers created

---
+++ src/client/reducers/filterRatingReducer.js
@@ -0,0 +1,13 @@
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'RATING_FILTER_ON':{
+            return [...state,action.payload]
+        }
+        case 'RATING_FILTER_OFF':{
+            return [
+                ...state
+            ].filter((rating)=>rating!=action.payload)
+        }
+        default : return state
+    }
+}
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux'
 import cartReducer from './cartReducer'
 import fetchDataReducer from'./fetchDataReducer'
 import filterCategoryReducer from'./filterCategoryReducer'
+import filterRatingReducer from'./filterRatingReducer'
+
 
 export default combineReducers({
     count: cartReducer,
     wholeData: fetchDataReducer,
-    categoryFilter: filterCategoryReducer
+    categoryFilter: filterCategoryReducer,
+    ratingFilter: filterRatingReducer,
 })
