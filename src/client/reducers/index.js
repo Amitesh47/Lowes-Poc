c84deaddc4182ef05194285e2026cb49501faba3
Anurag Hakeem <anuhakee@publicisgroupe.net>
2019-08-20 15:15:55 +0530

    Card Comp UI complete and filter Category reducer created

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import cartReducer from './cartReducer'
 import fetchDataReducer from'./fetchDataReducer'
+import filterCategoryReducer from'./filterCategoryReducer'
 
 export default combineReducers({
     count: cartReducer,
     wholeData: fetchDataReducer,
+    categoryFilter: filterCategoryReducer
 })
