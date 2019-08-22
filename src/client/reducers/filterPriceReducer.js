631784bcf363ba037aaa44f03c4906625e566f34
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-08-22 16:20:16 +0530

    Price filter done

--- src/client/reducers/filterPriceReducer.js
+++ src/client/reducers/filterPriceReducer.js
@@ -1,12 +1,13 @@
 export default (state = [], action) => {
     switch (action.type) {
         case 'PRICE_FILTER_ON':{
-            return [...state,action.payload]
+            console.log("Hello red")
+            return [...state,...action.payload]
         }
         case 'PRICE_FILTER_OFF':{
             return [
                 ...state
-            ].filter((price)=>price!=action.payload)
+            ].filter((price) => !action.payload.includes(price))
         }
         default : return state
     }
