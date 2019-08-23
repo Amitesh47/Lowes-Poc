623f9bb394e62b94308fb19849030c60ef28ca90
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-08-23 12:59:40 +0530

    Header component testing fixed

--- src/client/components/Header/index.test.js
+++ src/client/components/Header/index.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Header from './index';
+import { Provider } from 'react-redux'
+import configureMockStore from 'redux-mock-store'
+
+
+
+const mockStore = configureMockStore();
 
 export const findByTestAtrr = (component, attr) => {
     const wrapper = component.find(`[data-test='${attr}']`);
@@ -8,28 +14,47 @@ export const findByTestAtrr = (component, attr) => {
 };
 
 describe('Header Component', () => {
+    let store
+    beforeEach(() => {
+        store = mockStore({
+            count: { totalCount: 0 }
+        });
+    });
+
     it('Should render Header component without errors', () => {
-        const component = shallow(<Header/>);
+        const component = mount(
+            <Provider store={store}>
+                <Header />
+            </Provider>);
         const wrapper = findByTestAtrr(component, 'getHeader');
-        expect(wrapper.length).toBe(1);
+        expect(wrapper.length).toBe(3);
     });
 
     it('Should render Logo component without errors', () => {
-        const component = shallow(<Header/>);
+        const component = mount(
+        <Provider store={store}>
+            <Header />
+        </Provider>);
         const wrapper = findByTestAtrr(component, 'getLogo');;
-        expect(wrapper.length).toBe(1);
+        expect(wrapper.length).toBe(3);
     });
 
     it('Should render Search component without errors', () => {
-        const component = shallow(<Header/>);
+        const component = mount(
+            <Provider store={store}>
+            <Header />
+        </Provider>);;
         const wrapper = findByTestAtrr(component, 'getSearch');;
-        expect(wrapper.length).toBe(1);
+        expect(wrapper.length).toBe(3);
     });
 
     it('Should render Cart component without errors', () => {
-        const component = shallow(<Header/>);
+        const component = mount(
+            <Provider store={store}>
+            <Header />
+        </Provider>);;
         const wrapper = findByTestAtrr(component, 'getCart');
-        expect(wrapper.length).toBe(1);
+        expect(wrapper.length).toBe(3);
     });
-    
+
 })
