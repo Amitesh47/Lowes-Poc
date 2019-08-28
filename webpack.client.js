d5a85d46ccf1afbbc35146279fcf0e0c5877bf49
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-08-28 17:29:22 +0530

    modified package.json filr

--- webpack.client.js
+++ webpack.client.js
@@ -5,6 +5,7 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const baseConfig = require("./webpack.base")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const config = {
     mode:'development',
@@ -15,9 +16,10 @@ const config = {
     output:{
         filename: 'bundle.js',
         path: path.resolve(__dirname,'public')
-    }
+    },
     //tell webpack where to put the output file
     //that is generated
+    plugins: [new BundleAnalyzerPlugin()]
 }
 
 module.exports = merge(baseConfig,config);
